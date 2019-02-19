c12ef93dbf7744a424a61a11389cda8236e79094
frostmine <frostmine1@gmail.com>
2019-02-19 14:08:18 +0700

    first commit

---
+++ latihan1.py
@@ -0,0 +1,9 @@
+import random
+jumlah = int(input("Masukan Jumlahnya :"))
+print("*****************************")
+for i in range(jumlah):
+    i=random.uniform(0.0,0.5)
+    print("data ke: =>",i)
+print("*****************************")
+print("selesai")
+print("*****************************")
